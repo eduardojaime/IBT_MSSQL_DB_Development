df9191698759e5bb183601915c6101217858f779
Eduardo Jaime <eduardojaime86@gmail.com>
2021-11-01 13:44:50 -0400

    uploaded mock data scripts

---
+++ 2021 October/mock data/Customers.sql
@@ -0,0 +1,56 @@
+create table Customers (
+	id INT,
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	email VARCHAR(50)
+);
+insert into Customers (id, firstName, lastName, email) values (1, 'West', 'Attenbarrow', 'wattenbarrow0@ifeng.com');
+insert into Customers (id, firstName, lastName, email) values (2, 'Seka', 'Cannon', 'scannon1@gmpg.org');
+insert into Customers (id, firstName, lastName, email) values (3, 'Nobie', 'Aucoate', 'naucoate2@netvibes.com');
+insert into Customers (id, firstName, lastName, email) values (4, 'Hammad', 'Danilishin', 'hdanilishin3@businesswire.com');
+insert into Customers (id, firstName, lastName, email) values (5, 'Dov', 'Lisciardelli', 'dlisciardelli4@storify.com');
+insert into Customers (id, firstName, lastName, email) values (6, 'Aguistin', 'Fraczek', 'afraczek5@tuttocitta.it');
+insert into Customers (id, firstName, lastName, email) values (7, 'Prisca', 'McCrone', 'pmccrone6@odnoklassniki.ru');
+insert into Customers (id, firstName, lastName, email) values (8, 'Dun', 'Barkas', 'dbarkas7@unicef.org');
+insert into Customers (id, firstName, lastName, email) values (9, 'Krispin', 'Blenkinship', 'kblenkinship8@360.cn');
+insert into Customers (id, firstName, lastName, email) values (10, 'Priscella', 'Sweetmore', 'psweetmore9@quantcast.com');
+insert into Customers (id, firstName, lastName, email) values (11, 'Corenda', 'Exall', 'cexalla@dot.gov');
+insert into Customers (id, firstName, lastName, email) values (12, 'Binny', 'Loreit', 'bloreitb@hostgator.com');
+insert into Customers (id, firstName, lastName, email) values (13, 'Mozes', 'Schiesterl', 'mschiesterlc@meetup.com');
+insert into Customers (id, firstName, lastName, email) values (14, 'Angelica', 'Welland', 'awellandd@google.ca');
+insert into Customers (id, firstName, lastName, email) values (15, 'Hamil', 'Gair', 'hgaire@ucoz.com');
+insert into Customers (id, firstName, lastName, email) values (16, 'Corbie', 'MacNelly', 'cmacnellyf@google.es');
+insert into Customers (id, firstName, lastName, email) values (17, 'Laurena', 'Eynald', 'leynaldg@samsung.com');
+insert into Customers (id, firstName, lastName, email) values (18, 'Cele', 'Clampe', 'cclampeh@nbcnews.com');
+insert into Customers (id, firstName, lastName, email) values (19, 'Cordie', 'Dolle', 'cdollei@typepad.com');
+insert into Customers (id, firstName, lastName, email) values (20, 'Marthe', 'Bowness', 'mbownessj@cmu.edu');
+insert into Customers (id, firstName, lastName, email) values (21, 'Robbi', 'Ferris', 'rferrisk@canalblog.com');
+insert into Customers (id, firstName, lastName, email) values (22, 'Linell', 'Willmot', 'lwillmotl@nsw.gov.au');
+insert into Customers (id, firstName, lastName, email) values (23, 'Gay', 'Dorking', 'gdorkingm@epa.gov');
+insert into Customers (id, firstName, lastName, email) values (24, 'Amos', 'MacShirie', 'amacshirien@imgur.com');
+insert into Customers (id, firstName, lastName, email) values (25, 'Ewan', 'Willan', 'ewillano@ucla.edu');
+insert into Customers (id, firstName, lastName, email) values (26, 'Eustacia', 'Cotgrave', 'ecotgravep@cbsnews.com');
+insert into Customers (id, firstName, lastName, email) values (27, 'Gawain', 'Parramore', 'gparramoreq@amazon.co.uk');
+insert into Customers (id, firstName, lastName, email) values (28, 'Ray', 'Guierre', 'rguierrer@hugedomains.com');
+insert into Customers (id, firstName, lastName, email) values (29, 'Thomas', 'Segrave', 'tsegraves@who.int');
+insert into Customers (id, firstName, lastName, email) values (30, 'Vivyan', 'Densell', 'vdensellt@fda.gov');
+insert into Customers (id, firstName, lastName, email) values (31, 'Katuscha', 'Janovsky', 'kjanovskyu@shareasale.com');
+insert into Customers (id, firstName, lastName, email) values (32, 'Clementius', 'Bagot', 'cbagotv@odnoklassniki.ru');
+insert into Customers (id, firstName, lastName, email) values (33, 'Tremayne', 'Balser', 'tbalserw@unc.edu');
+insert into Customers (id, firstName, lastName, email) values (34, 'Avigdor', 'Oxtarby', 'aoxtarbyx@weibo.com');
+insert into Customers (id, firstName, lastName, email) values (35, 'Ethyl', 'Tombling', 'etomblingy@hubpages.com');
+insert into Customers (id, firstName, lastName, email) values (36, 'Jose', 'Guerro', 'jguerroz@t-online.de');
+insert into Customers (id, firstName, lastName, email) values (37, 'Jerad', 'Coulton', 'jcoulton10@pagesperso-orange.fr');
+insert into Customers (id, firstName, lastName, email) values (38, 'Fabian', 'Glanester', 'fglanester11@nsw.gov.au');
+insert into Customers (id, firstName, lastName, email) values (39, 'Prentiss', 'Dubber', 'pdubber12@geocities.jp');
+insert into Customers (id, firstName, lastName, email) values (40, 'Hermia', 'Hamor', 'hhamor13@behance.net');
+insert into Customers (id, firstName, lastName, email) values (41, 'Cletus', 'Lymer', 'clymer14@state.tx.us');
+insert into Customers (id, firstName, lastName, email) values (42, 'Yoshiko', 'Dolton', 'ydolton15@senate.gov');
+insert into Customers (id, firstName, lastName, email) values (43, 'Allyson', 'Pessold', 'apessold16@jugem.jp');
+insert into Customers (id, firstName, lastName, email) values (44, 'Blaire', 'Keeves', 'bkeeves17@ted.com');
+insert into Customers (id, firstName, lastName, email) values (45, 'Andy', 'Watson', 'awatson18@jugem.jp');
+insert into Customers (id, firstName, lastName, email) values (46, 'Sherwin', 'Vanstone', 'svanstone19@umn.edu');
+insert into Customers (id, firstName, lastName, email) values (47, 'Thekla', 'O''Dooghaine', 'todooghaine1a@wunderground.com');
+insert into Customers (id, firstName, lastName, email) values (48, 'Nathaniel', 'Glascott', 'nglascott1b@woothemes.com');
+insert into Customers (id, firstName, lastName, email) values (49, 'Aguste', 'McGray', 'amcgray1c@google.com');
+insert into Customers (id, firstName, lastName, email) values (50, 'Giulia', 'Birney', 'gbirney1d@aol.com');
